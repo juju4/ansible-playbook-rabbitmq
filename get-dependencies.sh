17c65c93ed062818b59e2df88277b0d04bd648f3
juju4 <juju4@users.noreply.github.com>
2017-01-07 05:10:29 -0500

    update with galaxy naming

---
+++ get-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+if [ $# != 0 ]; then
+rolesdir=$1
+else
+rolesdir=$(dirname $0)/..
+fi
+
+[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/Mayeu.RabbitMQ ] && ln -s ansible-playbook-rabbitmq $rolesdir/Mayeu.RabbitMQ
+[ ! -e $rolesdir/Mayeu.RabbitMQ ] && cp -R $rolesdir/ansible-playbook-rabbitmq $rolesdir/Mayeu.RabbitMQ
+
+## don't stop build on this script return code
+true
+
